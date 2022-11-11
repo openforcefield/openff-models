a0d00b1298a452a546f9b424c6df10e47ef6024b
Matthew W. Thompson <mattwthompson@protonmail.com>
2022-11-11 10:12:45 -0600

    Lint

--- openff/models/exceptions.py
+++ openff/models/exceptions.py
@@ -3,10 +3,12 @@ class MissingUnitError(ValueError):
     Exception for data missing a unit tag.
     """
 
+
 class UnitValidationError(ValueError):
     """
     Exception for bad behavior when validating unit-tagged data.
     """
+
 
 class UnsupportedExportError(BaseException):
     """
